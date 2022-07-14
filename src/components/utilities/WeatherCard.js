680dd446bbeb4224b49aa4830c75f356ff6ab7e8
mujaffar <mujaffarhssn@gmail.com>
2022-07-14 13:45:14 +0530

    placeholder img added in weather card img

--- src/components/utilities/WeatherCard.js
+++ src/components/utilities/WeatherCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./weatherCardStyle.css";
+import loader from '../../assets/loading.jpg'
 
 export default function WeatherCard({
   cityName,
@@ -25,7 +26,7 @@ export default function WeatherCard({
           <div id="summary">{weather_desc}</div>
         </div>
       </div>
-      <img className="weatherimg" alt="image1" src={icon} />
+      <img className="weatherimg" alt="image1" preload="eager" src={icon?(icon):(loader)} />
       <div className="infos">
         <div className="humidity">Humidity {humidity}%</div>
         <div className="windspeed">Wind Speed {wind_speed} km</div>
